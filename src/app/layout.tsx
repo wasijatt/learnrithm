128d89df4962d3aac99654736c938fca698967cc
Muhammad wasim <muhammadwasim5686@gmail.com>
2025-07-16 23:52:44 -0700

    final git with responsiveness and read me file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,18 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CustomCursor from "./Components/CustomCursor";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
-  title: "Learnrithmm ",
+  title: "Learnrithmm-Learning doesn't have to be Hard ",
   description: "Learning doesn't have to be Hard",
 };
 
